df749e4c9fe9f833518dab6d6bc16bae5e7ce6e1
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2023-08-24 15:15:22 +0200

    feat(node-agent): add mock return values

--- node-agent/workload-manager/src/grpc/scheduler_workload_service.rs
+++ node-agent/workload-manager/src/grpc/scheduler_workload_service.rs
@@ -1,6 +1,7 @@
 use tonic::{Request, Response, Status, Result};
 use tokio_stream::wrappers::ReceiverStream;
 use orka_proto::node_agent::{Empty, Workload, workload_service_server::WorkloadService, WorkloadSignal, WorkloadStatus};
+use crate::container::client::ContainerClient;
 
 pub struct WorkloadSvc {}
 
@@ -16,10 +17,27 @@ impl WorkloadService for WorkloadSvc {
     type CreateStream = ReceiverStream<Result<WorkloadStatus>>;
 
     async fn create(&self, request: Request<Workload>) -> Result<Response<Self::CreateStream>, Status> {
-        todo!()
+        // let image = request.into_inner().image;
+        // let mut client = ContainerClient::new("/var/run/containerd/containerd.sock").await.unwrap();
+        // let container = client.create(&image).await.unwrap().into_inner().container.unwrap();
+        let status = WorkloadStatus {
+            // name: container.id,
+            name: "test".to_string(),
+            status: 1,
+            resource: None,
+            message: "message".to_string(),
+        };
+        let (tx, rx) = tokio::sync::mpsc::channel(4);
+        tokio::spawn(async move {
+            loop {
+                tx.send(Ok(status.clone())).await.unwrap();
+                tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+            }
+        });
+        Ok(Response::new(ReceiverStream::new(rx)))
     }
 
-    async fn signal(&self, request: Request<WorkloadSignal>) -> Result<Response<Empty>, Status> {
-        todo!()
+    async fn signal(&self, _: Request<WorkloadSignal>) -> Result<Response<Empty>, Status> {
+        Ok(Response::new(Empty {}))
     }
 }
