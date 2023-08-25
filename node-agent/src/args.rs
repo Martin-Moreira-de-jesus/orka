b3568f21cec68333bc634f38989e333f7906f5f0
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2023-08-25 19:31:19 +0200

    feat(node-agent): add app bootstrap

---
+++ node-agent/src/args.rs
@@ -0,0 +1,32 @@
+//! Command-line arguments.
+use clap::Parser;
+use clap_verbosity_flag::{InfoLevel, Verbosity};
+
+/// Scheduler service for the Orka container orchestration system.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct CliArguments {
+    /// The address of the scheduler to connect the node agent to.
+    #[arg(long, default_value = "[::]", env)]
+    pub scheduler_address: String,
+
+    /// The port of the scheduler to connect the node agent to.
+    #[arg(long, default_value_t = 50051, env)]
+    pub scheduler_port: u16,
+
+    /// The address of the node agent for the scheduler to connect to.
+    #[arg(long, default_value = "[::]", env)]
+    pub node_agent_address: String,
+
+    /// The port of the node agent for the scheduler to connect to.
+    #[arg(long, default_value_t = 50052, env)]
+    pub node_agent_port: u16,
+
+    /// The address of the containerd socket.
+    #[arg(long, default_value = "/run/containerd/containerd.sock", env)]
+    pub containerd_socket: String,
+
+    /// Verbosity level.
+    #[command(flatten)]
+    pub verbose: Verbosity<InfoLevel>,
+}
--- node-agent/src/main.rs
+++ node-agent/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+mod args;
+
+use args::CliArguments;
+use clap::Parser;
+use orka_proto::scheduler_agent::lifecycle_service_client::LifecycleServiceClient;
+use tracing::{event, Level};
+use tracing_log::AsTrace;
+use uuid::Uuid;
+use orka_proto::scheduler_agent::ConnectionRequest;
+
+#[tokio::main]
+async fn main() {
+    let args = CliArguments::parse();
+
+    tracing_subscriber::fmt()
+        .with_max_level(args.verbose.log_level_filter().as_trace())
+        .init();
+
+    event!(
+        Level::INFO,
+        app_name = env!("CARGO_PKG_NAME"),
+        app_version = env!("CARGO_PKG_VERSION"),
+        "Starting",
+    );
+
+    event!(Level::INFO, "Arguments: {:?}", args);
+
+    let mut lifecycle_client = LifecycleServiceClient::connect(format!(
+        "http://{}:{}",
+        args.scheduler_address, args.scheduler_port
+    ))
+    .await
+    .expect("Failed to create lifecycle client");
+
+    lifecycle_client.join_cluster(ConnectionRequest {
+        id: Uuid::new_v4().to_string(),
+    }).await.expect("Failed to join cluster");
+
+    event!(Level::INFO, "Joined cluster");
 }
